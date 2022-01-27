833cc1030068513173ada56abbff87fbe06f750e
Carl <hildebrandt.carl@outlook.com>
2022-01-27 18:52:05 -0500

    Updated code

--- hapticsuit_simulator/Freyja-Simulator/scope_to_csv.m
+++
@@ -1,6 +0,0 @@
-T = table(ScopeData{1}.Values.Time, ScopeData{1}.Values.Data, ScopeData{2}.Values.data, ScopeData{3}.Values.data);
-T.Properties.VariableNames{1} = 'time';
-T.Properties.VariableNames{2} = 'fx';
-T.Properties.VariableNames{3} = 'fy';
-T.Properties.VariableNames{4} = 'fz';
-writetable(T,"c9.csv")
