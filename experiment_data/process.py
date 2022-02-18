c7c01784dc262bbc297fdd24fc0d6c1ed5363c49
Carl <hildebrandt.carl@outlook.com>
2022-02-18 17:20:07 -0500

    Found the magic numbers

--- experiment_data/process.py
+++ experiment_data/process.py
@@ -1,5 +1,6 @@
 import glob
 import bagpy
+import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,130 +9,214 @@ from bagpy import bagreader
 
 # Window filter that smooths the data and returns both the upper and lower bound for that window
 def window_filter(x, window_size=5):
-    upper_bound = np.array(pd.Series(x).rolling(window_size).max())[window_size-1:]
-    lower_bound = np.array(pd.Series(x).rolling(window_size).min())[window_size-1:]
-    mean = np.array(pd.Series(x).rolling(window_size).mean())[window_size-1:]
-    return upper_bound, lower_bound, mean
 
+    # Create the rolling window to have all the same data as the first point
+    window = np.full(window_size, x[0])
+
+    # Create the output data
+    mean_data   = np.zeros(len(x))
+    upper       = np.zeros(len(x))
+    lower       = np.zeros(len(x))
+
+    # Run through the array
+    for i, d in enumerate(x):
+        mean_data[i]    = np.average(window)
+        upper[i]        = mean_data[i] + np.std(window)
+        lower[i]        = mean_data[i] - np.std(window)
+
+        # Roll the window
+        window = np.roll(window, 1)
+        window[0] = d
+        
+    return mean_data, upper, lower
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--test_duration', type=int, default=600,                help='Test duration (s)')
+parser.add_argument('--wait_time',     type=int, default=10,                 help='Time (s) after computer control mode before you start the test')
+parser.add_argument('--folder',        type=str, default='Hover/third_run/', help='Folder where bag files are stored')
+parser.add_argument('--window_size',   type=int, default=100,                help='The size of the window filter')
+args = parser.parse_args()
 
 # Get all the files
-bag_files = glob.glob("Hover/*.bag")
+bag_files = glob.glob("{}*.bag".format(args.folder))
 
 # For each bag file
-for i, file_name in enumerate(bag_files):
+for bag_file_index, file_name in enumerate(bag_files):
+
+    # Init for each loop
+    figure_count = 0
 
     # Load the bag
-    print("Loading: {}".format(file_name))
+
     bag = bagreader(file_name)
 
     # get the list of topics
     print("Available topics: ")
     print(bag.topic_table)
 
-    print("Processing")
+    print("Working on: {}".format(file_name))
+    print("\tLoading data")
+
     # Read specific topics and convert to panda
     state_vector_msgs   = bag.message_by_topic("/current_state")
     rpyt_msgs           = bag.message_by_topic("/rpyt_command")
+    reference_msgs      = bag.message_by_topic("/reference_state")
+    mavros_state_msgs   = bag.message_by_topic("/mavros/state")
     state_vector_data   = pd.read_csv(state_vector_msgs)
     rpyt_data           = pd.read_csv(rpyt_msgs)
+    reference_data      = pd.read_csv(reference_msgs)
+    mavros_state_data   = pd.read_csv(mavros_state_msgs)
 
-    # Get the fields we are interested in from the current state
-    pos_time        = np.array(state_vector_data["Time"])
-    x_data          = np.array(state_vector_data["state_vector_0"])
-    z_data          = np.array(state_vector_data["state_vector_2"])
+    print("\tProcessing data")
 
-    # Get the fiels we are interested in from the rpyt data
-    command_time    = np.array(rpyt_data["Time"])
-    control_mode    = np.array(rpyt_data["ctrl_mode"])
-    pitch_data      = np.array(rpyt_data["pitch"])
-    thurst_data     = np.array(rpyt_data["thrust"]) 
+    # Get the position (3), velocity(3), and attiude (3)
+    state_vector_length = len(state_vector_data["state_vector_0"])
+    vehicle_data        = {}
+    vehicle_labels      = ["x_pos", "y_pos", "z_pos", "x_vel", "y_vel", "z_vel", "roll", "pitch", "yaw", "dt"]
+    vehicle_indices     = [0, 1, 2, 3, 4, 5, 6, 7, 8, 12]
+    for label, index in zip(vehicle_labels, vehicle_indices):
+        vehicle_data[label] = np.array(state_vector_data["state_vector_{}".format(index)])
+    vehicle_data["Time"] = np.array(state_vector_data["Time"])
 
-    # Convert from down to up
-    z_data = z_data * -1
+    # Convert the z position and velocity to be up
+    vehicle_data["z_pos"] = vehicle_data["z_pos"] * -1
+    vehicle_data["z_vel"] = vehicle_data["z_vel"] * -1
 
-    # Convert from Epoch time to seconds
-    pos_time = pos_time - pos_time[0]
-    command_time = command_time - command_time[0]
+    # Get the command data (4)
+    command_data        = {}
+    command_labels      = ["roll", "pitch", "yaw", "thrust"]
+    for label in command_labels:
+        command_data[label] = np.array(rpyt_data["{}".format(label)])
+    command_data["Time"] = np.array(rpyt_data["Time"])
 
-    # Find the times we are interested in (seconds)
-    start_time = 60
-    end_time = 480
-    end_time = end_time + start_time
+    # Get the setpoints 
+    setpoint_data        = {}
+    setpoint_labels      = ["x_pos", "y_pos", "z_pos", "x_vel", "y_vel", "z_vel"]
+    setpoint_names       = ["pn", "pe", "pd", "vn", "ve", "vd"]
+    for label, name in zip(setpoint_labels, setpoint_names):
+        setpoint_data[label] = np.array(reference_data["{}".format(name)])
+    setpoint_data["Time"] = np.array(reference_data["Time"])
 
-    p_start     = np.argmax(pos_time > start_time)
-    p_end       = np.argmax(pos_time > end_time)
-    c_start     = np.argmax(command_time > start_time)
-    c_end       = np.argmax(command_time > end_time)
+    # Convert the z position and velocity to be up
+    setpoint_data["z_pos"] = setpoint_data["z_pos"] * -1
+    setpoint_data["z_vel"] = setpoint_data["z_vel"] * -1
 
-    # Crop all data to start and end at right time
-    pos_time        = pos_time[p_start:p_end]
-    x_data          = x_data[p_start:p_end]
-    z_data          = z_data[p_start:p_end]
+    # Get the computer control mode data
+    control_mode_data    = {}
+    control_mode_labels  = ["armed", "mode"]
+    for label in control_mode_labels:
+        control_mode_data[label] = np.array(mavros_state_data["{}".format(label)])
+    control_mode_data["Time"] = np.array(mavros_state_data["Time"])
 
-    command_time    = command_time[c_start:c_end]
-    control_mode    = control_mode[c_start:c_end]
-    pitch_data      = pitch_data[c_start:c_end]
-    thurst_data     = thurst_data[c_start:c_end]
+    print("\tCropping only test data")
 
-    # Reset time to start at 0
-    pos_time = pos_time - pos_time[0]
-    command_time = command_time - command_time[0]
+    # Determine when the drone was first set to computer control mode
+    cc_time = 0
+    for i in range(len(control_mode_data["mode"])):
+        a = control_mode_data["armed"][i]
+        m = control_mode_data["mode"][i] 
+          
+        if (a) and ("CMODE(25)" in m):
+            # Define when computer control was actually started
+            cc_time = control_mode_data["Time"][i]
+            break 
+    
+    # Get the indices where to crop data from
+    start_time = cc_time + args.wait_time
+    end_time = cc_time + args.test_duration + args.wait_time
+    vehicle_crop_s_index = np.argmax(vehicle_data["Time"]>=start_time)
+    command_crop_s_index = np.argmax(command_data["Time"]>=start_time)
+    setpoint_crop_s_index = np.argmax(setpoint_data["Time"]>=start_time)
+    control_crop_s_index = np.argmax(control_mode_data["Time"]>=start_time)
 
-    # Smooth the data
-    win_size_p = 1000
-    win_size_c = 100
-    x_upper, x_lower, x_mean = window_filter(x_data, window_size=win_size_p)
-    z_upper, z_lower, z_mean = window_filter(z_data, window_size=win_size_p)
-    p_upper, p_lower, p_mean = window_filter(pitch_data, window_size=win_size_c)
-    t_upper, t_lower, t_mean = window_filter(thurst_data, window_size=win_size_c)
+    vehicle_crop_e_index = np.argmax(vehicle_data["Time"]>=end_time)
+    command_crop_e_index = np.argmax(command_data["Time"]>=end_time)
+    setpoint_crop_e_index = np.argmax(setpoint_data["Time"]>=end_time)
+    control_crop_e_index = np.argmax(control_mode_data["Time"]>=end_time)
 
-    # Crop the time to fit inside the filter
-    pos_time = pos_time[win_size_p-1:]
-    command_time = command_time[win_size_c-1:]
+    # Crop the data
+    for key in vehicle_data:
+        vehicle_data[key] = vehicle_data[key][vehicle_crop_s_index:vehicle_crop_e_index]
+    for key in command_data:
+        command_data[key] = command_data[key][command_crop_s_index:command_crop_e_index]
+    for key in setpoint_data:
+        setpoint_data[key] = setpoint_data[key][setpoint_crop_s_index:setpoint_crop_e_index]
+    for key in control_mode_data:
+        control_mode_data[key] = control_mode_data[key][control_crop_s_index:control_crop_e_index]
+       
+    # Convert to seconds
+    print("\tConverting from Unix time to seconds")
+    vehicle_data["Time"] = vehicle_data["Time"] - vehicle_data["Time"][0]
+    command_data["Time"] = command_data["Time"] - command_data["Time"][0]
+    setpoint_data["Time"] = setpoint_data["Time"] - setpoint_data["Time"][0]
+    control_mode_data["Time"] = control_mode_data["Time"] - control_mode_data["Time"][0]
 
-    # Generate a labl
-    label_text = ""
-    if "JOZI_10m_hover" in file_name:
-        label_text = "Base drone"
-    elif "JOZI_10m_weight" in file_name:
-        label_text = "Artificial Weight"
-    elif "SUIT" in file_name:
-        label_text = "Haptic Suit"
+    print("\tPlotting data")   
+    
+    # Plot the position data
+    time = vehicle_data["Time"]
 
-    # Plot the data
-    print("Plotting")
+    # X data
+    plt.figure(figure_count)
+    average_x, upper_x, lower_x = window_filter(vehicle_data["x_pos"], window_size=args.window_size)
+    plt.plot(time, average_x, c="C{}".format(bag_file_index), label=file_name)
+    plt.fill_between(time, lower_x, upper_x, color="C{}".format(bag_file_index), alpha=0.3)
+    figure_count += 1
 
-    plt.figure(3)
-    plt.plot(pos_time, x_mean, c="C{}".format(i), label=label_text)
-    plt.fill_between(pos_time, x_lower, x_upper, color="C{}".format(i), alpha=0.3)
-    plt.title("X Position")
-    plt.ylabel("Position (m)")
-    plt.xlabel("Time (s)")
-    plt.legend()
+    # Y data
+    plt.figure(figure_count)
+    average_y, upper_y, lower_y = window_filter(vehicle_data["y_pos"], window_size=args.window_size)
+    plt.plot(time, average_x, c="C{}".format(bag_file_index), label=file_name)
+    plt.fill_between(time, lower_y, upper_y, color="C{}".format(bag_file_index), alpha=0.3)
+    figure_count += 1
 
-    plt.figure(4)
-    plt.plot(pos_time, z_mean, c="C{}".format(i), label=label_text)
-    plt.fill_between(pos_time, z_lower, z_upper, color="C{}".format(i), alpha=0.3)
-    plt.title("Z Position")
-    plt.ylabel("Position (m)")
-    plt.xlabel("Time (s)")
-    plt.legend()
+    # Z data
+    plt.figure(figure_count)
+    average_z, upper_z, lower_z = window_filter(vehicle_data["z_pos"], window_size=args.window_size)
+    plt.plot(time, average_z, c="C{}".format(bag_file_index), label=file_name)
+    plt.fill_between(time, lower_z, upper_z, color="C{}".format(bag_file_index), alpha=0.3)
+    figure_count += 1
 
-    plt.figure(6)
-    plt.plot(command_time, p_mean, c="C{}".format(i), label=label_text)
-    plt.fill_between(command_time, p_lower, p_upper, color="C{}".format(i), alpha=0.3)
-    plt.title("Pitch Commands")
-    plt.ylabel("Pitch (?)")
-    plt.xlabel("Time (s)")
-    plt.legend()
 
-    plt.figure(7)
-    plt.plot(command_time, t_mean, c="C{}".format(i), label=label_text)
-    plt.fill_between(command_time, t_lower, t_upper, color="C{}".format(i), alpha=0.3)
-    plt.title("Thrust Commands")
-    plt.ylabel("Thrust (N)")
-    plt.xlabel("Time (s)")
-    plt.legend()
+figure_count = 0
 
-    print("----------------")
+# Plot the position setpoints data
+time = setpoint_data["Time"]
+
+# X data
+plt.figure(figure_count)
+data_x = setpoint_data["x_pos"]
+plt.plot(time, data_x, c="Black", label="Setpoint")
+plt.title("X Position")
+plt.ylabel("Position (m)")
+plt.xlabel("Time (s)")
+plt.ylim([-0.5, 0.5])
+plt.legend()
+figure_count += 1
+
+# Y data
+plt.figure(figure_count)
+data_y = setpoint_data["y_pos"]
+plt.plot(time, data_y, c="Black", label="Setpoint")
+plt.title("Y Position")
+plt.ylabel("Position (m)")
+plt.xlabel("Time (s)")
+plt.ylim([-0.5, 0.5])
+plt.legend()
+figure_count += 1
+
+# Z data
+plt.figure(figure_count)
+data_z = setpoint_data["z_pos"]
+plt.plot(time, data_z, c="Black", label="Setpoint")
+plt.title("Z Position")
+plt.ylabel("Position (m)")
+plt.xlabel("Time (s)")
+plt.ylim([0.25, 1.25])
+plt.legend()
+figure_count += 1
 plt.show()
+
+
+
+# # TODO Add vicon data to the plot
